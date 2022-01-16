89a54985bef565df34c85c86e8c379645af32d90
desarrollocml <cristianmirandalamas@gmail.com>
2022-01-15 23:46:11 -0300

    test-GET

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 import express from "express";
+import { v4 } from "uuid";
 
 const app = express();
 
+const tasks = [];
+
+//Middlewares
+app.use(express.json());
+
+//Routes
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
+
+app.get("/tasks", (req, res) => {
+  res.status(200).json(tasks);
+});
+
+app.post("tasks", (req, res) => {
+  const { DataTransferItemList, description } = req.body;
+  if (!title || !description) return res.sendStatus(400);
+
+  const newTask = { ...req.body, id: v4() };
+  tasks.push(newTask);
+  res.status(200).json(newTask);
+});
+
 export default app;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,10 +1,17 @@
 import request from "supertest";
 import app from "../src/app";
 
+//set NODE_OPTIONS= --experimental-vm-modules && npx jest
 describe("GET /tasks", () => {
   test("should respond width a 200 status code", async () => {
     const response = await request(app).get("/tasks").send();
-    console.log(response);
+    expect(response.statusCode).toBe(200);
   });
+
+  test("should respond an array", async ()=>{
+      const response = await request(app).get("/tasks").send();
+      expect(response.body).toBeInstanceOf(Array)
+  })
+
 });
-//set NODE_OPTIONS= --experimental-vm-modules && npx jest
+
